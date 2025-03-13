9ae4fc453370496c46c71e92803b666f67979246
getel-arch <getelmusic@gmail.com>
2025-03-13 16:41:36 +0200

    Update check_debugged.c

--- src/check_debugged.c
+++ src/check_debugged.c
@@ -4,15 +4,48 @@
 
 #pragma comment(lib, "ntdll.lib")
 
-int main() {
+BOOL CheckDebuggedUsingPEB() {
+    
     // Get the address of the PEB
     PEB* peb = (PEB*)__readgsqword(0x60);
 
-    // Check the BeingDebugged value
-    if (peb->BeingDebugged) {
-        printf("The process is being debugged.\n");
+    return peb->BeingDebugged;
+}
+
+BOOL CheckDebuggedUsingAPI() {
+    BOOL isDebuggerPresent = FALSE;
+    CheckRemoteDebuggerPresent(GetCurrentProcess(), &isDebuggerPresent);
+    return isDebuggerPresent;
+}
+
+int main() {
+    if (argc != 2) {
+        printf("Usage: %s <method_number>\n", argv[0]);
+        printf("Method numbers:\n");
+        printf("1 - PEB method\n");
+        printf("2 - CheckRemoteDebuggerPresent method\n");
+        return 1;
+    }
+
+    int method = atoi(argv[1]);
+    BOOL isDebuggerPresent = FALSE;
+
+    switch (method) {
+        case 1:
+            isDebuggerPresent = CheckDebuggedUsingPEB();
+            break;
+        case 2:
+            isDebuggerPresent = CheckDebuggedUsingAPI();
+            break;
+        default:
+            printf("Invalid method number. Use 1 or 2.\n");
+            return 1;
+    }
+
+    if (isDebuggerPresent) {
+        printf("The process is being debugged (method %d).\n", method);
     } else {
-        printf("The process is not being debugged.\n");
+        printf("The process is not being debugged (method %d).\n", method);
     }
 
     return 0;
